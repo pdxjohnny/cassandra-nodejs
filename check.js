ab57f5b4acf7a2b8ccca62df5b445fe92b7ff198
John Andersen <johnandersenpdx@gmail.com>
2015-05-23 11:55:51 -0700

    First commit

---
+++ check.js
@@ -0,0 +1,15 @@
+var cassandra = require('cassandra-driver');
+
+var client = new cassandra.Client({ contactPoints: ['127.0.0.1']});
+
+client.connect(function (err) {
+  if (err) {
+    client.shutdown();
+    return console.error('There was an error when connecting', err);
+  }
+  console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+  console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
+  client.shutdown();
+});
+
+
